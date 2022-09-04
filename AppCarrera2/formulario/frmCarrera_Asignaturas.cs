312d291d246429f4d4b2a6ac22632099f3d95fb3
NicolásAgustinRame <109234443+NicolasAgustinRame@users.noreply.github.com>
2022-09-03 23:00:25 -0300

    .

---
+++ AppCarrera2/formulario/frmCarrera_Asignaturas.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppCarrera.Formularios
+{
+    public partial class frmCarrera_Asignaturas : Form
+    {
+        public frmCarrera_Asignaturas()
+        {
+            InitializeComponent();
+        }
+    }
+}
